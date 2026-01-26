a891c92fac149bedc045ccfe362e85c57a0fd013
Thibaut rey <thibautrey@me.com>
2026-01-26 15:45:05 +0100

    fix(i18n): replace i18next-react with react-i18next and update dependencies

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,5 +1,5 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'i18next-react';
+import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import fr from './locales/fr.json';
 import en from './locales/en.json';
