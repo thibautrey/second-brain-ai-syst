97b962f09dd003e7768bc6d3203dde2feb4504cd
Thibaut Rey <thibautrey@me.com>
2026-01-28 14:30:10 +0100

    fix

--- backend/prisma/migrations/20260128_add_audio_storage/migration.sql
+++ backend/prisma/migrations/20260128_add_audio_storage/migration.sql
@@ -7,6 +7,6 @@ ALTER TABLE "negative_examples" ADD COLUMN "audioData" BYTEA;
 ALTER TABLE "negative_examples" ADD COLUMN "audioMimeType" TEXT;
 ALTER TABLE "negative_examples" ADD COLUMN "durationSeconds" DOUBLE PRECISION;
 
--- Create index on admittedAt for easier cleanup of old records
-CREATE INDEX "adaptive_samples_admittedAt_idx" ON "adaptive_samples"("admittedAt");
+-- Create index on capturedAt for easier cleanup of old records
+-- Note: adaptive_samples_admittedAt_idx already created in previous migration
 CREATE INDEX "negative_examples_capturedAt_idx" ON "negative_examples"("capturedAt");
